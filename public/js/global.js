811a89e3ba194ad9b1319adb8fa7af00698eb350
sabarisreact <133691399+sabarisreact@users.noreply.github.com>
2024-03-30 10:37:39 +0530

    css changes

--- public/js/global.js
+++ public/js/global.js
@@ -34,8 +34,6 @@
 	}
 
 	window.addEventListener('keydown', handleFirstTab);
-
-	// Fit slide video background to video holder
 	function resizeVideo() {
 		var $holder = $( '.videoHolder' );
 		$holder.each( function() {
@@ -75,15 +73,12 @@
 				else {
 					$buttonWrapper.show();
 				}
-				// console.log(resp.paged);
 			},
 			error: function( err ) {
 				console.log( err.textStatus );
 			}
 		} );
 	}
-
-	// Load more
 	$( document ).on( 'click', '#loadmore', function( e ) {
 		var $insightsList = $( '.insights__list .grid-x' );
 		var $this = $( this ), $buttonWrapper = $this.closest( '.posts-list__more' );
@@ -111,9 +106,6 @@
 		AjaxNew(data, $buttonWrapper, $insightsList);
 	} );
 
-
-
-	// Init Jquery UI select
 	function initJUIselect(elem) {
 		var $field = $( elem );
 		var $gfield = $field.closest( ".gfield" );
@@ -127,10 +119,7 @@
 		}
 		$field.selectmenu( args ).on('change',function() {
 			$(this).selectmenu('refresh');
-		});
-		
-// 		for newspaper page dont want jquery ui
-		
+		});		
 		$( "#jquerynotselect" ).selectmenu( "destroy" );
 	}
 
@@ -142,11 +131,9 @@
 		} );
 	}
 
-	// Scripts which runs after DOM load
 	var scrollOut;
 	$( document ).ready(function() {
         AOS.init({once: true});
-		// Init LazyLoad
 		var lazyLoadInstance = new LazyLoad({
 			elements_selector: 'img[data-lazy-src],.pre-lazyload',
 			data_src: "lazy-src",
@@ -156,9 +143,7 @@
 			class_loading: "lazyloading",
 			class_loaded: "lazyloaded",
 		});
-		// Add tracking on adding any new nodes to body to update lazyload for the new images (AJAX for example)
 		window.addEventListener('LazyLoad::Initialized', function (e) {
-			// Get the instance and puts it in the lazyLoadInstance variable
 			if (window.MutationObserver) {
 				var observer = new MutationObserver(function(mutations) {
 					mutations.forEach(function(mutation) {
@@ -179,49 +164,12 @@
 				observer.observe(b, config);
 			}
 		}, false);
-
-		// Load all images in slider after init
 		$( document ).on( "init", ".slick-slider", function( e, slick ) {
 			lazyLoadInstance.loadAll( slick.$slider[0].getElementsByTagName( 'img' ) );
 		} );
-
-		// Detect element appearance in viewport
-		scrollOut = ScrollOut( {
-			offset: function() {
-				return window.innerHeight - 200;
-			},
-			once: true,
-			onShown: function( element ) {
-				if ( $( element ).is( '.ease-order' ) ) {
-					$( element ).find( '.ease-order__item' ).each( function( i ) {
-						var $this = $( this );
-						$( this ).attr( 'data-scroll', '' );
-						window.setTimeout( function() {
-							$this.attr( 'data-scroll', 'in' );
-						}, 300 * i );
-					} );
-				}
-			}
-		} );
-
-
-		// Init parallax
-		/*$('.jarallax').jarallax({
-			speed: 0.5,
-		});
-
-		$('.jarallax-inline').jarallax({
-			speed: 0.5,
-			keepImg: true,
-			onInit : function() { lazyLoadInstance.update(); }
-		});*/
-
-		// IE Object-fit cover polyfill
 		if ( $( '.of-cover, .stretched-img' ).length ) {
 			objectFitImages( '.of-cover, .stretched-img' );
 		}
-
-		//Remove placeholder on click
 		$( 'input,textarea' ).each( function() {
 			$( this ).data( 'holder', $( this ).attr( 'placeholder' ) );
 
@@ -233,8 +181,6 @@
 				$( this ).attr( 'placeholder', $( this ).data( 'holder' ) );
 			} );
 		} );
-
-		// Init Jquery UI select
 		$( "select" ).not( "#billing_state, #shipping_state, #billing_country, #shipping_country, [class*='woocommerce'], #product_cat" ).each( function() {
 			initJUIselect(this);
 		} );
@@ -244,27 +190,6 @@
 				initJUIselect(this)
 			} );
 		});
-
-
-
-		//Make elements equal height
-		$( '.matchHeight' ).matchHeight();
-
-
-		// Add fancybox to images
-		$( '.gallery-item' ).find('a[href$="jpg"], a[href$="png"], a[href$="gif"]').attr( 'rel', 'gallery' ).attr( 'data-fancybox', 'gallery' );
-		$( 'a[rel*="album"], .fancybox, a[href$="jpg"], a[href$="png"], a[href$="gif"]' ).fancybox( {
-			minHeight: 0,
-			helpers: {
-				overlay: {
-					locked: false
-				}
-			}
-		} );
-
-		/**
-		 * Scroll to Gravity Form confirmation message after form submit
-		 */
 		$( document ).on( 'gform_confirmation_loaded', function( event, formId ) {
 			var $target = $( '#gform_confirmation_wrapper_' + formId );
 			if ( $target.length ) {
@@ -274,10 +199,6 @@
 				return false;
 			}
 		} );
-
-		/**
-		 * Hide gravity forms required field message on data input
-		 */
 		$( 'body' ).on( 'change keyup', '.gfield input, .gfield textarea, .gfield select', function() {
 			var $field = $( this ).closest( '.gfield' );
 			if ( $field.hasClass( 'gfield_error' ) && $( this ).val().length ) {
@@ -286,20 +207,12 @@
 				$field.find( '.validation_message' ).show();
 			}
 		} );
-
-		/**
-		 * Add `is-active` class to menu-icon button on Responsive menu toggle
-		 * And remove it on breakpoint change
-		 */
 		$( window ).on( 'toggled.zf.responsiveToggle', function() {
 			$( '.menu-icon' ).toggleClass( 'is-active' );
 		} ).on( 'changed.zf.mediaquery', function( e, value ) {
 			$( '.menu-icon' ).removeClass( 'is-active' );
 		} );
 
-		/**
-		 * Close responsive menu on orientation change
-		 */
 		$( window ).on( 'orientationchange', function() {
 			setTimeout( function() {
 				if ( $( '.menu-icon' ).hasClass( 'is-active' ) && window.innerWidth < 641 ) {
@@ -309,45 +222,27 @@
 		} );
 
 		resizeVideo();
-
-		/*
-		*  This function will render each map when the document is ready (page has loaded)
-		*/
-
 		$('.acf-map').each(function(){
 			render_map( $(this) );
 		});
 
 	} );
-
-
-	// Scripts which runs after all elements load
-
 	$( window ).on( 'load', function() {
 
 		if ( typeof scrollOut !== "undefined" ) {
 			scrollOut.update();
 		}
-
-		//jQuery code goes here
 		if ( $( '.preloader' ).length ) {
 			$( '.preloader' ).addClass( 'preloader--hidden' );
 		}
 
 	} );
-
-	// Scripts which runs at window resize
 	var resizeSelectCallback = debounce( resizeSelect, 200 );
 	$( window ).on( 'resize', function() {
-
-		//jQuery code goes here
-
 		resizeVideo();
 		resizeSelectCallback();
 		
 	} );
-
-	// Scripts which runs on scrolling
 	$(document).ready(function() {
 		$(document).on('click','#clickBtn', function(e) {
 			$(this).next('#popup').css('display', 'block');
@@ -358,19 +253,7 @@
 		});
 	});
 
-
-	// $(window).scroll(function() {
-	// 	var winTop = $(window).scrollTop();
-	// 	if (winTop >= 1) {
-	// 		$(".header").addClass("header-fixed");
-	// 	} else{
-	// 		$(".header").removeClass("header-fixed");
-	// 	}
-	// });
-
 	$( window ).on( 'scroll', function() {
-
-		//jQuery code goes here
 		$('.related-posts').slick({
 			dots: false,
 			infinite: true,
@@ -388,130 +271,8 @@
 		});
 	} );
 
-
-	// $('.post-block__block').slick({
-	// 	dots: false,
-	// 	infinite: true,
-	// 	speed: 300,
-	// 	slidesToShow:  2,
-	// 	slidesToScroll: 1,
-	// 	appendArrows: $('.post-block-arrow'),
-	// 	prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-	// 		'<path d="M2.2002 12L5.8701 9V12V15L2.2002 12Z" fill="white"/>\n' +
-	// 		'<path d="M23.2002 12H2.2002M2.2002 12L5.8701 9V15L2.2002 12Z" stroke="white" stroke-width="1.5" stroke-linecap="square"/>\n' +
-	// 		'</svg>\n</button>',
-	// 	nextArrow: '<button id="next" type="button" class="btn btn-juliet"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-	// 		'<path d="M22 12L18.3301 9L18.3301 12L18.3301 15L22 12Z" fill="white"/>\n' +
-	// 		'<path d="M0.999999 12L22 12M22 12L18.3301 9L18.3301 15L22 12Z" stroke="white" stroke-width="1.5" stroke-linecap="square"/>\n' +
-	// 		'</svg>\n</button>',
-	// 	responsive: [
-	// 		{
-	// 			breakpoint: 1440,
-	// 			settings: {
-	// 				slidesToShow: 1,
-	// 				slidesToScroll: 1
-	// 			}
-	// 		},
-	// 		{
-	// 			breakpoint: 1025,
-	// 			settings: {
-	// 				slidesToShow: 2,
-	// 				slidesToScroll: 1
-	// 			}
-	// 		},
-	// 		{
-	// 			breakpoint: 641,
-	// 			settings: {
-	// 				slidesToShow: 1,
-	// 				slidesToScroll: 1
-	// 			}
-	// 		}
-	// 	]
-	// });
-
-	/*$('.testimonials__img').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 6000,
-        draggable:true,
-        arrows: false,
-		asNavFor: '.testimonials__content',
-		centerMode: true,
-		centerPadding: '0px',
-		responsive: [
-			{
-				breakpoint: 641,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
-	});
-	$('.testimonials__content').slick({
-		slidesToShow: 1,
-        autoplay: true,
-        autoplaySpeed: 6000,
-		slidesToScroll: 1,
-		asNavFor: '.testimonials__img',
-		dots: false,
-		responsive: [
-			{
-				breakpoint: 641,
-				settings: {
-					arrows: false,
-				}
-			}
-		]
-	});*/
-
     const $rootSingle = $('.testimonials__content');
     const $rootNav = $('.testimonials__img');
-    /**/
-    // $rootSingle.slick({
-    //     slide: '.testimonials__content-block',
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    //     autoplay: true,
-    //     autoplaySpeed: 6000,
-    //     arrows: true,
-    //     dots: false,
-    //     responsive: [
-    //         {
-    //             breakpoint: 641,
-    //             settings: {
-    //                 arrows: false,
-    //             }
-    //         }
-    //     ]
-    // });
-
-    // $rootNav.on('init', function(event, slick) {
-    //     $(this).find('.slick-slide.slick-current').addClass('is-active');
-    // })
-    //     .slick({
-    //         slide: '.testimonials__img-item',
-    //         slidesToShow: 3,
-    //         slidesToScroll: 1,
-    //         autoplay: true,
-    //         autoplaySpeed: 6000,
-    //         centerMode: true,
-    //         draggable:true,
-    //         centerPadding: '0px',
-    //         dots: false,
-    //         arrows: false,
-    //         responsive: [
-    //             {
-    //                 breakpoint: 641,
-    //                 settings: {
-    //                     slidesToShow: 1,
-    //                     slidesToScroll: 1
-    //                 }
-    //             }
-    //         ]
-    //     });
-
     $rootSingle.on('afterChange', function(event, slick, currentSlide) {
         $rootNav.slick('slickGoTo', currentSlide);
         $rootNav.find('.slick-slide.is-active').removeClass('is-active');
@@ -560,102 +321,53 @@
         $('html, body').stop().animate({
             'scrollTop': $target.offset().top
         }, 900, 'swing', function () {
-            // window.location.hash = target;
         });
 
     });
 
-
-    /*
-     *  This function will render a Google Map onto the selected jQuery element
-     */
-
 	function render_map( $el ) {
-		// var
 		var $markers = $el.find( '.marker' );
-		// var styles = []; // Uncomment for map styling
-
-		// vars
 		var args = {
 			zoom: 16,
 			center: new google.maps.LatLng( 0, 0 ),
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			scrollwheel: false,
-			// styles : styles // Uncomment for map styling
 		};
-
-		// create map
 		var map = new google.maps.Map( $el[0], args );
-
-		// add a markers reference
 		map.markers = [];
-
-		// add markers
 		$markers.each( function() {
 			add_marker( $( this ), map );
 		} );
-
-		// center map
 		center_map( map );
 	}
-
-	/*
-	 *  This function will add a marker to the selected Google Map
-	 */
-
 	var infowindow;
 
 	function add_marker( $marker, map ) {
-		// var
 		var latlng = new google.maps.LatLng( $marker.attr( 'data-lat' ), $marker.attr( 'data-lng' ) );
-
-		// create marker
 		var marker = new google.maps.Marker( {
 			position: latlng,
 			map: map,
-			//icon: $marker.data('marker-icon') //uncomment if you use custom marker
 		} );
-
-		// add to array
 		map.markers.push( marker );
-
-		// if marker contains HTML, add it to an infoWindow
 		if ( $.trim( $marker.html() ) ) {
-			// create info window
 			infowindow = new google.maps.InfoWindow();
-
-			// show info window when marker is clicked
 			google.maps.event.addListener( marker, 'click', function() {
-				// Close previously opened infowindow, fill with new content and open it
 				infowindow.close();
 				infowindow.setContent( $marker.html() );
 				infowindow.open( map, marker );
 			} );
 		}
 	}
-
-	/*
-	*  This function will center the map, showing all markers attached to this map
-	*/
-
 	function center_map( map ) {
-		// vars
 		var bounds = new google.maps.LatLngBounds();
-
-		// loop through all markers and create bounds
 		$.each( map.markers, function( i, marker ) {
 			var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
 			bounds.extend( latlng );
 		} );
-
-		// only 1 marker?
 		if ( map.markers.length == 1 ) {
-			// set center of map
 			map.setCenter( bounds.getCenter() );
 		} else {
-			// fit to bounds
 			map.fitBounds( bounds );
 		}
 	}
-
 }( jQuery ));
