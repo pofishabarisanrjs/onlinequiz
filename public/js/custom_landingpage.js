bd871f6ab17c504bb65a1adab4fc36eada2d9d65
sabarisreact <133691399+sabarisreact@users.noreply.github.com>
2024-04-02 21:10:41 +0530

    commit

--- public/js/custom_landingpage.js
+++ public/js/custom_landingpage.js
@@ -1,4 +1,4 @@
-var myFullpage = new fullpage('#fullpage', {
+var myFullpage = new fullpage('#quizpage', {
 			//anchors: ['hero','video_popup','scantojoin','question1','answer1','video_popup_2','scancode','question2','answer2','slide1','slide2','slide3','slide4','slide5','slide6','slide7','slide8','slide9','slide10','slide11','slide12','question3','answer3','slide13'],
 			sectionsColor: ['#0A0909', '#0A0909', '#0A0909','#0A0909', '#0A0909','#0A0909','#0A0909','#0A0909','#0A0909','#0A0909','#0A0909','#171616','#0A0909','#171616','#0A0909','#171616','#0A0909','#171616','#0A0909','#171616','#0A0909','#171616','#0A0909','#171616'],
 			// navigation: false,
@@ -15,6 +15,27 @@ var myFullpage = new fullpage('#fullpage', {
 			// 	}
 // 			navigationTooltips: ['First page', 'Second page', 'Third and last page']
 		});
+	  // Disable mouse scroll and swipe gestures for the element with ID 'scrollable'
+	  var scrollableElement = document.getElementById('quizpage');
+
+	  scrollableElement.addEventListener('wheel', function(e) {
+		e.preventDefault();
+	  });
+	
+	  var startX, startY;
+	
+	  scrollableElement.addEventListener('touchstart', function(e) {
+		var touch = e.touches[0];
+		startX = touch.clientX;
+		startY = touch.clientY;
+	  });
+	
+	  scrollableElement.addEventListener('touchmove', function(e) {
+		if (Math.abs(startX - e.touches[0].clientX) > 10 ||
+			Math.abs(startY - e.touches[0].clientY) > 10) {
+		  e.preventDefault();
+		}
+	  });
 	
 		$(document).ready(function () {
 			$('.popup-youtube').YouTubePopUp(); // Initialize Magnific Popup for YouTube videos
