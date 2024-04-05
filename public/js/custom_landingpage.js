50d4d885fff222972e44b6a49c0589db967702e6
sabarisreact <133691399+sabarisreact@users.noreply.github.com>
2024-04-05 18:15:01 +0530

    commit

--- public/js/custom_landingpage.js
+++ public/js/custom_landingpage.js
@@ -26,12 +26,61 @@ var myFullpage = new fullpage('#quizpage', {
     formContainer.addEventListener('wheel', function(e) {
         e.stopPropagation(); // Stop the event from bubbling up
     });
-	document.addEventListener('touchmove', function(event) {
-		event.preventDefault();
-	}, { passive: false });
-	formContainer.addEventListener('touchmove', function(e) {
-        e.stopPropagation(); // Stop the event from bubbling up
-    });
+	// document.addEventListener('touchmove', function(event) {
+	// 	event.preventDefault();
+	// }, { passive: false });
+	// formContainer.addEventListener('touchmove', function(e) {
+    //     e.stopPropagation(); // Stop the event from bubbling up
+    // });
+	
+	document.addEventListener('DOMContentLoaded', function() {
+		var currentSection = 1;
+		toggleButtons(currentSection);
+	
+		document.getElementById('prevButton').addEventListener('click', function() {
+			if (currentSection > 1) {
+				currentSection--;
+				toggleButtons(currentSection);
+				scrollToSection(currentSection);
+			}
+		});
+	
+		document.getElementById('nextButton').addEventListener('click', function() {
+			if (currentSection < 22) {
+				currentSection++;
+				toggleButtons(currentSection);
+				scrollToSection(currentSection);
+			}
+		});
+	
+		// Function to show or hide prev/next buttons based on current section
+		function toggleButtons(currentSection) {
+			console.log(currentSection)
+			var prevBtn = document.getElementById('prevButton');
+			var nextBtn = document.getElementById('nextButton');
+	
+			if (currentSection === 1) {
+				prevBtn.style.display = 'none';
+			} else {
+				prevBtn.style.display = 'block';
+			}
+	
+			if (currentSection === 22) {
+				nextBtn.style.display = 'none';
+			} else {
+				nextBtn.style.display = 'block';
+			}
+		}
+	
+		// Function to scroll to a specific section
+		function scrollToSection(sectionNumber) {
+			var sectionId = 'section' + sectionNumber;
+			var section = document.getElementById(sectionId);
+			if (section) {
+				section.scrollIntoView({ behavior: 'smooth' });
+			}
+		}
+	});
 	
 		$(document).ready(function () {
 			$('.popup-youtube').YouTubePopUp(); // Initialize Magnific Popup for YouTube videos
